3c71c496cba034ef7c338c4cff0ef1cd9abeb547
Vitche Research Team Developer <developer@vitche.com>
2023-10-18 02:38:54 +0300

    - style fixes. - a test for a single "process" call.

--- source.py
+++ source.py
@@ -5,38 +5,45 @@ import asyncio
 import time
 
 
+# TODO: How to stop a task?
 class TransactionsSource:
+
     def __init__(self, ws_url, rpc, transaction_filter):
         self.ws_url = ws_url
         self.web3 = Web3(Web3.HTTPProvider(rpc))
         self.filter = transaction_filter
         self.data = {}
-        self.process_task = None
+        self.task = None
 
-    def process(self):
-        if self.process_task is None:
-            self.process_task = asyncio.create_task(self.__process())
+    def process(self, _):
 
-        else:
-            return [self.data]
+        # Create a task if not exists
+        if self.task is None:
+            self.task = asyncio.create_task(self.__process())
+
+        return self.data
 
     async def __process(self):
+
         async with connect(self.ws_url) as ws:
+
             await ws.send(
                 '{"jsonrpc": "2.0", "id": 1, "method": "eth_subscribe", "params": ["newPendingTransactions"]}')
             subscription_response = await ws.recv()
             print(subscription_response)
 
             while True:
+
                 message = await asyncio.wait_for(ws.recv(), timeout=15)
                 response = json.loads(message)
                 if 'result' in response['params']:
+
                     tx_hash = response['params']['result']
 
-                    tx_info = self.web3.eth.get_transaction(tx_hash)
-                    if tx_info is not None:
-                        if self.filter(tx_info):
-                            to_address = tx_info['to']
+                    transaction = self.web3.eth.get_transaction(tx_hash)
+                    if transaction is not None:
+                        if self.filter(transaction):
+                            to_address = transaction['to']
                             current_time = int(time.time())
 
                             if to_address not in self.data:
@@ -44,5 +51,5 @@ class TransactionsSource:
 
                             self.data[to_address].append({
                                 "time": current_time,
-                                "value": tx_info
+                                "value": transaction
                             })
--- tests/test_source.py
+++ tests/test_source.py
@@ -11,6 +11,7 @@ class TestSource(unittest.TestCase):
         self.loop = asyncio.get_event_loop()
 
     def test_initialize(self):
+
         source = TransactionsSource(
             "wss://dex.binance.org/api/ws",
             "https://bsc-dataseed.binance.org",
@@ -18,15 +19,16 @@ class TestSource(unittest.TestCase):
 
         self.assertTrue(source is not None)
 
-    def test_process(self):
+    def test_single_process(self):
+
         source = TransactionsSource(
             "wss://dex.binance.org/api/ws",
             "https://bsc-dataseed.binance.org",
             contract_deployment)
 
         async def process():
-            data = source.process()
-            print(data)
+            data = source.process([])
+            self.assertTrue(data == {})
 
         self.loop.run_until_complete(process())
 
